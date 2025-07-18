13e1910511a917932517ba7d33c6a6b6f1f29183
Luisk000 <luishenriqueangelini@gmail.com>
2025-07-18 19:09:55 -0300

    exemplo react native elements

--- app/cronograma.tsx
+++ app/cronograma.tsx
@@ -1,12 +1,25 @@
 import { Text, View } from "react-native";
 import { useTheme } from "react-native-paper";
 
+import { Button } from '@rneui/themed';
+//import { Button } from 'react-native-paper';
+
+
 export default function Cronograma() {
 const theme = useTheme();
     
   return (
     <View  style={{ backgroundColor: theme.colors.primary }} >
       <Text>Tela de cronograma</Text>
+
+      {/* Botão do React Native Element: */}
+      <Button title='Teste' containerStyle={{ width: 200 }}></Button>
+
+      {/* Botão do React Native Paper: */}
+      {/* <Button icon="camera" mode="contained">
+        Camera
+      </Button> */}
+
     </View>
   );
 }
